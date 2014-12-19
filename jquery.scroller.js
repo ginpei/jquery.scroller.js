90ae6c407251e0ee2ae6efabbf41e86ffcc24080
ginpei <ginpei@gmail.com>
2014-12-19 21:25:12 +0900

    scrolltoend event

--- jquery.scroller.js
+++ jquery.scroller.js
@@ -4,10 +4,33 @@ jQuery.scroller = (function(document) {
 	var html = document.documentElement;
 	var body = document.body;
 
-	$(document).on('scroll', function(event) {
+	scroller.onscroll = function(event) {
 		scroller.top = body.scrollTop || html.scrollTop;
 		scroller.trigger('scroll', event);
+
+		if (this.isOnBottom()) {
+			scroller.trigger('scrolltoend', event);
+		}
+	};
+
+	scroller.isOnBottom = function() {
+		return (this.top + this.windowHeight >= this.pageHeight);
+	};
+
+	scroller.updateWindowInformations = function(event) {
+		this.pageHeight = body.scrollHeight;
+		this.windowHeight = html.clientHeight;
+	};
+
+	$(document).on('scroll', function(event) {
+		scroller.onscroll(event);
 	});
+	$(window).on('resize', function(event) {
+		scroller.updateWindowInformations(event);
+	});
+
+	// initialize
+	scroller.updateWindowInformations();
 
 	return scroller;
 })(document);
